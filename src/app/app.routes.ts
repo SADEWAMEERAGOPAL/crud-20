15f5aa8053dc93d59537f890c870ee0b5c68a8cd
SADEWAMEERAGOPAL <SADEWADMEERA76@GMAIL.COM>
2026-04-19 20:35:54 +0530

    navigation problem fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,20 @@ import { PostDetails } from './component/post-details/post-details';
 import { PostForm } from './component/post-form/post-form';
 import { PageNotFound } from './component/page-not-found/page-not-found';
 
-export const routes: Routes = [{
-    path: "",
-    component: PostDashboard
-},
+export const routes: Routes = [
+
+
 {
     path: "posts",
     component: PostDashboard
 },
+
+ {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+
 {
     path: "posts/addpost",
     component: PostForm
